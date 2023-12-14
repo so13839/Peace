07d6d8eac6bfc870cb3cd1f118b2cffa1fe5e0de
Stephen Opiyo <so13839@gmail.com>
2023-12-14 15:21:07 -0500

    Updated_Regression.R

--- Regression.R
+++ Regression.R
@@ -1,7 +1,7 @@
 #Mul_Reg_data <- read_excel("~/BIA7105-24/Mul_Reg_data.xlsx")
 dim(Mul_Reg_data) 
 str(Mul_Reg_data)
-#string is for looking at the structure of the files
+#string is for looking at the structure of the files, and other materials.
 Mul_Reg_data$Gender <- as.factor(Mul_Reg_data$Gender)
 Mul_Reg_data$Department <- as.factor(Mul_Reg_data$Department)
 Mul_Reg_Result <- lm(Salary~., data = Mul_Reg_data)
